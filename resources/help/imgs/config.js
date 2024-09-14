4711b7faba30945e039e3eaff2516527d51ba38e
maple-leaf-sweeping <197728340@qq.com>
2024-09-14 08:12:26 +0800

    细节优化

--- resources/help/imgs/config.js
+++ resources/help/imgs/config.js
@@ -3,7 +3,7 @@ export const style = {
   fontColor: '#ceb78b',
   // 主文字阴影： 横向距离 垂直距离 阴影大小 阴影颜色
   // fontShadow: '0px 0px 1px rgba(6, 21, 31, .9)',
-  fontShadow: 'none',
+  fontShadow: '0px 0px 1px rgba(6, 21, 31, .9)',
   // 描述文字颜色
   descColor: '#eee',
 
@@ -13,7 +13,7 @@ export const style = {
   contBgColor: 'rgba(6, 21, 31, .5)',
 
   // 面板底图毛玻璃效果，数字越大越模糊，0-10 ，可为小数
-  contBgBlur: 3,
+  contBgBlur: 5,
 
   // 板块标题栏底色
   headerBgColor: 'rgba(6, 21, 31, .4)',
