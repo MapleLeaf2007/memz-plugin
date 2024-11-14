1250c0f6106954eb9510d5aef6d4405f642b45dd
yeshang2007 <197728340@qq.com>
2024-11-14 14:51:41 +0800

    refactor(RedisStatus): 修改 Redis 状态 PRO 的输出格式
    - 移除了 Redis 实例配置信息
    - 调整了输出信息的格式，使其更加简洁

--- apps/RedisStatus.js
+++ apps/RedisStatus.js
@@ -159,7 +159,7 @@ ${dbStats.databaseStats}`;
   }
 
   generateProTextResponse(stats, hitRate, dbStats, redisConfig) {
-    return `Redis 实例: ${redisConfig}
+    return `Redis 状态PRO
 已运行天数: ${stats.uptime_in_days} days
 当前监听端口: ${stats.tcp_port}
 连接的客户端数量: ${stats.connected_clients}
