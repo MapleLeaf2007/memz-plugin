59bf57f804e9187f972bf9619438d090b623f330
maple-leaf-sweeping <197728340@qq.com>
2024-11-22 10:39:50 +0800

    refactor: 删除未使用的插件代码
    - 移除了 Cili.js 和 SearchForResources.js 两个插件文件
    - 这些插件可能在未来的某个时刻以重构的形式重新加入项目

--- apps/Search.js
+++ apps/Search.js
@@ -62,6 +62,10 @@ export class ResourceSearchPlugin extends plugin {
         {
           reg: '^#?资源(分类|类别)?(统计|数量|总数|总计)$',
           fnc: 'handleCategoryCount'
+        },
+        {
+          reg: '^#?磁力搜索\\s*(.+)',
+          fnc: 'fetchSearchResults'
         }
       ]
     })
@@ -115,6 +119,50 @@ export class ResourceSearchPlugin extends plugin {
       await e.reply(message)
     } catch (error) {
       await e.reply(`统计过程中发生错误：${error.message}`, true)
+    }
+  }
+
+  async fetchSearchResults (e) {
+    const { SearchMagnet } = Config.getConfig('memz')
+    if (!SearchMagnet && !e.isMaster) return logger.warn('[memz-plugin]磁力搜索状态当前为仅主人可用')
+    const msg = e.msg
+    const searchQuery = msg.match(/^#?磁力搜索\s*(.+)$/)[1]
+    if (!searchQuery) {
+      return await e.reply('请输入有效的搜索关键词！', true)
+    }
+    const url = `https://cili.site/search?q=${encodeURIComponent(searchQuery)}`
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error('请求失败，状态码：' + response.status)
+      }
+      const data = await response.text()
+      const results = []
+      const regex = /<tr>[\s\S]*?<td>[\s\S]*?<a href="([^"]+)">[\s\S]*?<p class="sample">([^<]+)<\/p>[\s\S]*?<\/a>[\s\S]*?<\/td>[\s\S]*?<td class="td-size">([^<]+)<\/td>/g
+      let match
+      while ((match = regex.exec(data)) !== null) {
+        const link = `https://cili.site${match[1]}`
+        const title = match[2].trim()
+        const size = match[3].trim()
+        results.push({ title, size, link })
+      }
+
+      if (results.length > 0) {
+        const forward = results.map((row) => ({
+          user_id: 2173302144,
+          nickname: '为什么不玩原神',
+          message: [
+            `名称: ${row.title}\n文件大小: ${row.size}\n下载链接: ${row.link}`
+          ]
+        }))
+        const nmsg = await Bot.makeForwardMsg(forward)
+        await e.reply(nmsg)
+      } else {
+        await e.reply('未找到匹配的资源。', true)
+      }
+    } catch (error) {
+      logger.error('获取数据时出错:', error)
+      await e.reply('搜索过程中发生错误，请稍后再试。', true)
     }
   }
 }
