3dcafd740d35af968e62ef18c06f1d74fc146e50
maple-leaf-sweeping <197728340@qq.com>
2024-11-22 10:45:29 +0800

    refactor(WebTools): 将网页截图功能从 ScreenshotOfTheWebpage 移至 WebTools
    - 删除 ScreenshotOfTheWebpage.js 文件
    - 在 WebTools.js 中添加网页截图功能
    - 优化代码结构，避免重复功能

--- apps/WebTools.js
+++ apps/WebTools.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import whois from 'whois-json'
 import { generateScreenshot } from '../model/generateScreenshot.js'
 import { Config, PluginPath } from '../components/index.js'
-
+import puppeteer from '../../../lib/puppeteer/puppeteer.js'
 const whoisFieldsMap = {
   domainName: '域名',
   roid: '注册号',
@@ -134,6 +134,10 @@ export class WebTools extends plugin {
         {
           reg: '^#?whois\\s*(.+)',
           fnc: 'whois'
+        },
+        {
+          reg: '^#?网页截图\\s*(\\S+.*)',
+          fnc: 'webpage'
         }
       ]
     })
@@ -235,5 +239,53 @@ export class WebTools extends plugin {
     } catch (error) {
       await this.reply(`错误: ${error.message}`, true)
     }
+  }
+
+  async webpage (e) {
+    const { webpage } = Config.getConfig('memz')
+    if (!webpage && !e.isMaster) {
+      return logger.warn('[memz-plugin] 网页截图状态当前为仅主人可用')
+    }
+
+    const url = this.extractUrlFromMessage(e.msg)
+    if (!url) {
+      return await e.reply('请输入有效的网址', true)
+    }
+
+    try {
+      const screenshotBase64 = await this.captureScreenshot(url)
+      await e.reply(segment.image(`base64://${screenshotBase64}`), true)
+    } catch (error) {
+      await e.reply(`网页截图失败: ${error.message}`, true)
+    }
+  }
+
+  extractUrlFromMessage (message) {
+    let url = message.match(/^#?网页截图\s*(\S+.*)/)?.[1].trim()
+    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'http://' + url
+    }
+    return url
+  }
+
+  async captureScreenshot (url) {
+    if (!puppeteer.browser) {
+      await puppeteer.browserInit()
+    }
+    const page = await puppeteer.browser.newPage()
+
+    await page.setViewport({ width: 1920, height: 1080 })
+
+    await page.goto(url, { waitUntil: 'load' })
+
+    const screenshotBase64 = await page.screenshot({
+      fullPage: true,
+      type: 'jpeg',
+      quality: 100,
+      encoding: 'base64'
+    })
+
+    await page.close()
+    return screenshotBase64
   }
 }
