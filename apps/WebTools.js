eea58f156125fd6c44e777e8e4e0bec848f4181f
maple-leaf-sweeping <197728340@qq.com>
2024-11-22 10:37:42 +0800

    refactor(WebTools): 整合 WHOIS 查询功能
    - 将 Whois 插件的代码合并到 WebTools 插件中
    - 优化了 WHOIS 查询的代码结构
    - 更新了 WebTools 插件的配置和规则
    - 删除了单独的 Whois 插件文件

--- apps/WebTools.js
+++ apps/WebTools.js
@@ -1,5 +1,41 @@
-import { Config } from '../components/index.js'
+import fs from 'fs'
+import whois from 'whois-json'
+import { generateScreenshot } from '../model/generateScreenshot.js'
+import { Config, PluginPath } from '../components/index.js'
 
+const whoisFieldsMap = {
+  domainName: '域名',
+  roid: '注册号',
+  domainStatus: '域名状态',
+  registrant: '注册人信息',
+  registrantContactEmail: '注册人邮箱',
+  sponsoringRegistrar: '注册商',
+  nameServer: 'DNS 服务器',
+  registrationTime: '注册时间',
+  expirationTime: '过期时间',
+  dnssec: 'DNSSEC',
+  domain: '域名',
+  registrar: '注册商',
+  whois_server: 'WHOIS 服务器',
+  referral_url: '推荐 URL',
+  updated_date: '更新日期',
+  creation_date: '创建日期',
+  expiration_date: '过期日期',
+  status: '状态',
+  nameservers: 'DNS 服务器',
+  admin: '管理员信息',
+  tech: '技术联系人信息',
+  name: '姓名',
+  organization: '组织',
+  street: '街道',
+  city: '城市',
+  state: '省/州',
+  postal_code: '邮政编码',
+  country: '国家',
+  phone: '电话',
+  fax: '传真',
+  email: '电子邮件'
+}
 export async function fetchSeoFromHtml (url) {
   const response = await fetch(url)
   const html = await response.text()
@@ -58,11 +94,28 @@ export function decodeFromAscii (asciiStr) {
     String.fromCharCode(parseInt(match.replace('\\x', ''), 16))
   )
 }
+async function getDetailedWhoisData (domain) {
+  try {
+    return await whois(domain, { timeout: 10000 })
+  } catch (error) {
+    throw new Error(`获取 WHOIS 数据时出错: ${error.message}`)
+  }
+}
+function translateWhoisData (data) {
+  return Object.entries(data).reduce((acc, [key, value]) => {
+    const translatedKey = whoisFieldsMap[key] || key
+    acc[translatedKey] =
+      typeof value === 'object' && !Array.isArray(value)
+        ? translateWhoisData(value)
+        : value
+    return acc
+  }, {})
+}
 export class WebTools extends plugin {
   constructor () {
     super({
-      name: 'seo',
-      dsc: '抓取网站SEO信息',
+      name: 'WebTools',
+      dsc: 'WebTools',
       event: 'message',
       priority: 6,
       rule: [
@@ -77,6 +130,10 @@ export class WebTools extends plugin {
         {
           reg: /^(#?)(unicode|ascii)(编码|解码)\s*(.+)/,
           fnc: 'handleEncodingDecoding'
+        },
+        {
+          reg: '^#?whois\\s*(.+)',
+          fnc: 'whois'
         }
       ]
     })
@@ -155,5 +212,28 @@ export class WebTools extends plugin {
       reg: /^(#?)(unicode|ascii)(编码|解码)\s*(.+)/,
       fn: this.unicodehandleReply
     })
+  }
+
+  async whois (e) {
+    const { WhoisAll } = Config.getConfig('memz')
+    if (!WhoisAll && !e.isMaster) { return logger.warn('[memz-plugin]Whois状态当前为仅主人可用') }
+    const domain = e.msg.match(/#?whois\s*(.+)/)[1].trim()
+    try {
+      const data = await getDetailedWhoisData(domain)
+      const translatedData = translateWhoisData(data)
+
+      const whoisDataHtml = Object.entries(translatedData)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join('<br>')
+
+      const htmlTemplate = fs.readFileSync(`${PluginPath}/resources/html/whois/whois.html`, 'utf8')
+      const html = htmlTemplate.replace('{{whoisdata}}', whoisDataHtml)
+
+      const screenshotBuffer = await generateScreenshot(html)
+
+      await e.reply(segment.image(`base64://${screenshotBuffer}`), true)
+    } catch (error) {
+      await this.reply(`错误: ${error.message}`, true)
+    }
   }
 }
